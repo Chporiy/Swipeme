ab15e026671651e182d4a682f275cbb55b17aa12
Chporiy <vostok43kull@yandex.ru>
2020-07-13 08:27:28 +0300

    Фиксирую модальные окна

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -414,23 +414,23 @@ $(document).ready(function () {
   //   document.documentElement.msrequestFullscreen();
   // }
 
-  function toggleFullScreen() {
-    var doc = window.document;
-    var docEl = doc.documentElement;
+  // function toggleFullScreen() {
+  //   var doc = window.document;
+  //   var docEl = doc.documentElement;
     
-    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen;
-    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen;
+  //   var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen;
+  //   var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen;
     
-    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement) { 
-      requestFullScreen.call(docEl);
-    }
-    else {
-      cancelFullScreen.call(doc);  
-    }
-  }
+  //   if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement) { 
+  //     requestFullScreen.call(docEl);
+  //   }
+  //   else {
+  //     cancelFullScreen.call(doc);  
+  //   }
+  // }
 
   // $('body').on('click', () => {
-    toggleFullScreen()
+    // toggleFullScreen()
   // });
   
 
